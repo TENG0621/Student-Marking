e0f382d19b11c253ae274cac4a3b949b715aad50
TENG0621 <84773772+TENG0621@users.noreply.github.com>
2022-08-28 00:43:11 +0800

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,98 @@
+
+
+
+
+from cmath import sqrt
+import statistics
+import math 
+
+i = 1
+b = []
+mark_list=[]
+while True:   
+    a = []
+
+    student_number = i
+    i += 1
+    a.append(student_number)
+
+    student_name = input("\nPlease enter name : ")
+    a.append(student_name)
+    
+    student_id = int(input("Enter student's ID :"))
+    a.append(student_id)
+
+    student_mark = int(input("Enter student's mark : "))
+    mark_list.append(student_mark) #make a list for student mark 
+    a.append(student_mark)
+
+    if student_mark <= 100 and student_mark >= 80:
+        student_grade = "A"
+        a.append(student_grade)
+
+    elif student_mark <= 79 and student_mark >=60:
+        student_grade = "B"
+        a.append(student_grade)
+    
+    elif student_mark <= 59 and student_mark >= 50:
+        student_grade = "C"
+        a.append(student_grade)
+
+    elif student_mark <= 49 and student_mark >= 0:
+        student_grade = 'D'
+        a.append(student_grade)
+
+    print(a)
+    b.append(a)
+
+    again = input("\nDo you want to make a another record ? [y/n] :")
+    if again == "y":
+        continue
+    else:
+        break 
+
+
+# #for the calculation purpose 
+# x = len(b)
+# total = sum(mark_list)
+# average = float(total/x)
+# standard_deviation = float(statistics.pstdev(mark_list))
+# variance = sqrt(float(standard_deviation))
+
+# print("The average marks is : " + str(average))
+# print("The standard deviation is : " + str(standard_deviation))
+# print("The variance is : " + str(variance))
+
+#for the deleting of the student info 
+while True: 
+    for x in b:
+        print (x)
+        
+    delete_student = int(input("Please Choose the number of student that you wish to delete : "))
+
+    if student_number >= delete_student:
+        index = delete_student - 1
+        b.pop(index)
+        continue
+    else:
+        print("Invalid")
+        print (student_number)
+        print(delete_student)
+        break
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        
+
+
